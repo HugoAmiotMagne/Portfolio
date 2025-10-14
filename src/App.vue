d8a1028a71f44b5777e1e9f3a0b1f34a858c4618
h.amiotmagne <hugo463324@gmail.com>
2025-10-14 16:54:21 +0200

    Premier commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <RouterLink to="/">Home</RouterLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/pages/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Home View</h1>
+</template>
+
+<script setup>
+// tu peux ajouter du code ici plus tard
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
